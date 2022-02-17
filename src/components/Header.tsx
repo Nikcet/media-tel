9e994d6301b815139d5f59f86c76e18a0c01ceaa
Nikcet <mstislav.kr@yandex.ru>
2022-02-17 03:19:45 +0300

    Данные в правом верхнем углу появляются соотв. образом

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
-export default function Header() {
+import React from "react";
+import { UserContext } from "../contexts/UserContext";
+
+export default function Header({ ...props }) {
+  const currentUser = React.useContext(UserContext);
   return (
     <div className="header">
       <div className="header__wrap">
         <div className="header__account">
-          <p className="header__account-name">Иванов Иван Иванович</p>
+          <p className="header__account-name">{props.isLoggedIn ? currentUser.fio : "Аноним"}</p>
         </div>
         <button className="header__btn-logout" type="button">
-          Выйти
+          {props.isLoggedIn ? "Выйти" : "Привет"}
         </button>
       </div>
     </div>
